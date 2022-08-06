0a17041513efa6e69ca0b6c7a605165144e77eda
Luiz Henrique <luizhjramos@outlook.com>
2022-08-06 20:49:55 -0300

    chore(app): global fonts

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -10,4 +10,10 @@ export const GlobalStyle = createGlobalStyle`
     background: #333;
     color: #FFF;
   }
+
+  body, input, textarea, button {
+    font-family: 'Roboto', sans-serif;
+    font-weight: 400;
+    font-size: 1rem;
+  }
 `;
