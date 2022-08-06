ddf8dd1231f57d5dc1613ecef04c6eb5e4f4cfb6
Luiz Henrique <luizhjramos@outlook.com>
2022-08-06 20:34:17 -0300

    feat(app): styled-components application theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,6 @@
+import { ThemeProvider } from "styled-components"
+import { defaultTheme } from "./styles/themes/default";
+
 export function App() {
-  return <h1>Hello World</h1>;
+  return <ThemeProvider theme={defaultTheme}>Hello World</ThemeProvider>;
 }
