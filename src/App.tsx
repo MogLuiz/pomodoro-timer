24aba02a5a729be02255bc6cb98eeab055f3e40c
Luiz Henrique <luizhjramos@outlook.com>
2022-08-06 20:37:05 -0300

    styles(app): global styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,12 @@
-import { ThemeProvider } from "styled-components"
+import { ThemeProvider } from "styled-components";
+import { GlobalStyle } from "./styles/global";
 import { defaultTheme } from "./styles/themes/default";
 
 export function App() {
-  return <ThemeProvider theme={defaultTheme}>Hello World</ThemeProvider>;
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      Hello World
+      <GlobalStyle />
+    </ThemeProvider>
+  );
 }
