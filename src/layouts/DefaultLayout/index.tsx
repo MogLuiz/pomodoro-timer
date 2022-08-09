905390215e1c01827cd5601cc9ab29d6bb1b7e65
Luiz Henrique <luizhjramos@outlook.com>
2022-08-08 22:12:52 -0300

    refactor(layouts): change layouts folder structure

--- src/layouts/DefaultLayout/index.tsx
+++ src/layouts/DefaultLayout/index.tsx
@@ -1,9 +1,11 @@
 import { Outlet } from 'react-router-dom'
-import { Header } from '../components/Header'
+import { Header } from '../../components/Header'
+
+import * as S from './styles'
 
 export const DefaultLayout = () => (
-  <>
+  <S.Wrapper>
     <Header />
     <Outlet />
-  </>
+  </S.Wrapper>
 )
