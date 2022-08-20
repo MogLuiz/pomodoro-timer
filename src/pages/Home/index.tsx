c020fa8ad51ef97162f676c4f27f9418d7198ded
Luiz Henrique <luizhjramos@outlook.com>
2022-08-20 18:16:37 -0300

    feat: task input list suggestion

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -7,7 +7,18 @@ export const Home = () => (
     <form>
       <S.FormContainer>
         <label htmlFor="task">Vou trabalhar em</label>
-        <S.TaskInput id="task" placeholder="Dê um nome para o seu projeto" />
+        <S.TaskInput
+          id="task"
+          list="task-suggestions"
+          placeholder="Dê um nome para o seu projeto"
+        />
+
+        <datalist id="task-suggestions">
+          <option value="Projeto 1" />
+          <option value="Projeto 2" />
+          <option value="Projeto 3" />
+          <option value="Beterraba" />
+        </datalist>
 
         <label htmlFor="minutesAmount">durante</label>
         <S.MinutesAmountInput
