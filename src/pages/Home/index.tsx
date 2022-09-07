e6444dcaade73a8b32e7d523b3df6ef680780152
Luiz Henrique <luizhjramos@outlook.com>
2022-09-07 18:51:57 -0300

    feat: changing page title according to minutes and seconds

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -71,6 +71,12 @@ export const Home = () => {
   const minutes = String(minutesAmount).padStart(2, '0')
   const seconds = String(secondsAmount).padStart(2, '0')
 
+  useEffect(() => {
+    if (activeCycle) {
+      document.title = `${minutes}:${seconds}`
+    }
+  }, [minutes, seconds, activeCycle])
+
   const taskField = watch('task')
   const hasFormDisabled = !taskField
 
