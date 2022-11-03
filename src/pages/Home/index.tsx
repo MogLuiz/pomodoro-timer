53eddc6fe47bd3c594ec852d1624df5f3fbb477e
Luiz Henrique <luizhjramos@outlook.com>
2022-11-02 22:07:41 -0300

    refactor: modularize NewCycleForm for a single component

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -8,6 +8,7 @@ import { useForm } from 'react-hook-form'
 import { zodResolver } from '@hookform/resolvers/zod'
 
 import * as S from './styles'
+import { NewCycleForm } from './components/NewCycleForm'
 
 type TCycle = {
   id: string
@@ -116,35 +117,7 @@ export const Home = () => {
   return (
     <S.HomeContainer>
       <form onSubmit={handleSubmit(handleCreateNewCicle)}>
-        <S.FormContainer>
-          <label htmlFor="task">Vou trabalhar em</label>
-          <S.TaskInput
-            id="task"
-            list="task-suggestions"
-            placeholder="Dê um nome para o seu projeto"
-            {...register('task')}
-          />
-
-          <datalist id="task-suggestions">
-            <option value="Projeto 1" />
-            <option value="Projeto 2" />
-            <option value="Projeto 3" />
-            <option value="Beterraba" />
-          </datalist>
-
-          <label htmlFor="minutesAmount">durante</label>
-          <S.MinutesAmountInput
-            type="number"
-            id="minutesAmount"
-            placeholder="00"
-            step={5}
-            min={1}
-            max={60}
-            {...register('minutesAmount', { valueAsNumber: true })}
-          />
-
-          <span>minutos.</span>
-        </S.FormContainer>
+        <NewCycleForm />
 
         <S.CountdownContainer>
           <span>{minutes[0]}</span>
--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -14,51 +14,6 @@ export const HomeContainer = styled.main`
   }
 `
 
-export const FormContainer = styled.div`
-  ${({ theme }) => css`
-    width: 100%;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    gap: 0.5rem;
-    color: ${theme['gray-100']};
-    font-size: 1.125rem;
-    font-weight: bold;
-    flex-wrap: wrap;
-  `}
-`
-const BaseInput = styled.input`
-  ${({ theme }) => css`
-    background: transparent;
-    height: 2.5rem;
-    border: 0;
-    border-bottom: 2px solid ${theme['gray-500']};
-    font-weight: bold;
-    font-size: 1.125rem;
-    padding: 0 0.5rem;
-    color: ${theme['gray-100']};
-    &:focus {
-      box-shadow: none;
-      border-color: ${theme['green-500']};
-    }
-    &::placeholder {
-      color: ${theme['gray-500']};
-    }
-  `}
-`
-
-export const TaskInput = styled(BaseInput)`
-  flex: 1;
-
-  &::-webkit-calendar-picker-indicator {
-    display: none !important;
-  }
-`
-
-export const MinutesAmountInput = styled(BaseInput)`
-  width: 4rem;
-`
-
 export const CountdownContainer = styled.div`
   ${({ theme }) => css`
     font-family: 'Roboto Mono', monospace;
